9e265a9cbf62742fe8709a96c34b3d0efe47c9c7
ChidiRNweke <chidi125@gmail.com>
2025-05-18 09:33:44 +0200

    Remove deprecated parse.py file after refactoring into organized modules

--- autopdfparse/parse.py
+++
@@ -1,6 +0,0 @@
-# This file has been deleted as its contents have been moved to
-# better organized modules:
-# - exceptions.py: All exception classes
-# - models.py: Data models for the parsed results
-# - parsers/base.py: PDFParser protocol
-# - parsers/openai_parser.py: OpenAI parser implementation
